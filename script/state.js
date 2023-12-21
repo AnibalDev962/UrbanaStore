63eb22587462077ccaa0f063debada828fee1f18
AnibalDev962 <anibalpalacios320@outlook.com>
2023-12-20 20:27:18 -0600

    beging developing componenet

--- script/state.js
+++ script/state.js
@@ -4,8 +4,6 @@ import * as viewData from './view/view.js';
 
 export let state={
     ///SLIDER COMPONENT//
-    curSlide:0,
-    maxSlide: viewData.slides.length,
-    autoSlide:true, 
+   
 };
 
--- script/view/view.js
+++ script/view/view.js
@@ -1,6 +1,2 @@
 ///SLIDER COMPONENT//
 
-export const slides=document.querySelectorAll('.slider__slide');
-export const dotContainer=document.querySelector('.dots');
-export const btnLeft = document.querySelector('.slider__btn--left');
-export const btnRight = document.querySelector('.slider__btn--right');
