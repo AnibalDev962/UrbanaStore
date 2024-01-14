fb7f23e9ce59293155ec287c8400d0de2f7aee5f
AnibalDev962 <anibalpalacios320@outlook.com>
2024-01-14 15:51:02 -0600

    data base completed

--- script/products/productsDataBase.js
+++ script/products/productsDataBase.js
@@ -4,8 +4,103 @@
 export const objectTest={
     //structure explanation//1st value in array to find by gender, 2nd value to fing by sublink, 3rd value product name, 4th price, 5th unist avail, 6th image path. from 7on these are tags
     //0:gender,1:sub-selection,2product name.3.price,4.items avail. 5'img path'.6. all products/
-      1:['forkids','kids-clothes','doted t-shirt',22,4,'/c-1.0fc0df5c.jpeg','all-products'],
-      2:['forkids','kids-clothes','square design',22,4,'/c-2.f9d6181b.jpeg/','all-products'],
+      1:['forkids','kid-clothes','doted t-shirt',22,4,'/c-1.0fc0df5c.jpeg','all-products','selected-products'],
+      2:['forkids','kid-clothes','square design',22,4,'/c-2.f9d6181b.jpeg/','all-products','selected-products'],
+      3:['forkids','kid-clothes','name',22,24,'/c-3.7d2e7dff.jpeg','all-products','selected-products'],
+      4:['forkids','kid-clothes','name',34,22,'/c-6.de28f386.jpeg','all-products'],
+      5:['forkids','kid-clothes','name',200,24,'/c-8.7ca7a6e6.jpeg','all-products'],
+      6:['forkids','kid-clothes','name',200,24,'/c-10.9edb6553.jpg','all-products'],
+      7:['forkids','kid-clothes','name',200,24,'/c-9.39fccc53.jpeg','all-products'],
+
+      8:['firkids','kid-shoes','name',22,33,'/s-1.4f32e499.jpg','all-products','selected-products'],
+      9:['forkids','kid-shoes','name',22,33,'s-2.84e86a21.jpg','all-products','selected-products'],
+      10:['forkids','kid-shoes','name',22,33,'/s-3.4afdc058.jpg','all-products'],
+      11:['forkids','kid-shoes','name',22,33,'/s-4.f821138d.jpg','all-products'],
+
+      12:['forMen','men-clothes','name',33,44,'/c-1.29bc5455.jpg','all-products','selected-products'],
+      13:['forMen','men-clothes','name',33,44,'/c-2.56b86784.jpg','all-products','selected-products'],
+      14:['forMen','men-clothes','name',33,44,'/c-3.6dbe3f0b.jpg','all-products','selected-products'],
+      15:['forMen','men-clothes','name',33,44,'/c-4.c11a7158.jpg','all-products'],
+      16:['forMen','men-clothes','name',33,44,'/c-5.ea2e9ca3.jpg','all-products'],
+      17:['forMen','men-clothes','name',33,44,'/c-6.926bacac.jpg','all-products'],
+      18:['forMen','men-clothes','name',33,44,'/p-1.c504d534.jpg','all-products'],
+
+      19:['forMen','men-glasses','name',33,44,'/g-1.42d0bb27.jpg','all-products'],
+      21:['forMen','men-glasses','name',33,44,'/g-3.566c793d.jpg','all-products'],
+      22:['forMen','men-glasses','name',33,44,'/g-2.2c4d1062.jpg','all-products','selected-products'],
+      20:['forMen','men-glasses','name',33,44,'/g-4.a51c0f7f.jpg','all-products','selected-products'],
+    
+
+      22:['forMen','men-jewelry','name',33,44,'/j-1.68b772d8.jpg','all-products'],
+      23:['forMen','men-jewelry','name',33,44,'/j-2.c6f0688f.jpg','all-products'],
+      24:['forMen','men-jewelry','name',33,44,'/j-3.a5041868.jpg','all-products'],
+
+      25:['forMen','men-shoes','name',33,44,'/boots-1.e804de01.jpeg','all-products'],
+      26:['forMen','men-shoes','name',33,44,'/boots2.fe000089.jpeg','all-products','selected-products'],
+      27:['forMen','men-shoes','name',33,44,'/nike-1.0ca2ee8c.jpeg','all-products','selected-products'],
+      28:['forMen','men-shoes','name',33,44,'/nike-2.6159b1d0.jpeg','all-products','selected-products'],
+      29:['forMen','men-shoes','name',33,44,'/nike-3.d883d2bd.jpeg','all-products','selected-products'],
+      30:['forMen','men-shoes','name',33,44,'/nike-4.85a38bb5.webp','all-products'],
+      31:['forMen','men-shoes','name',33,44,'/nike-5.97f616db.jpeg','all-products'],
+      32:['forMen','men-shoes','name',33,44,'/shoe-2.68c72318.jpg','all-products'],
+      33:['forMen','men-shoes','name',33,44,'/vans-1.83622d80.jpg','all-products'],
+
+      34:['forMen','men-watches','name',33,44,'/w-1.e20f7505.jpg','all-products'],
+      35:['forMen','men-watches','name',33,44,'/w-2.0e56515c.jpg','all-products'],
+      36:['forMen','men-watches','name',33,44,'/w-3.f8bde621.jpg','all-products','selected-products'],
+      37:['forMen','men-watches','name',33,44,'/w-5.e7f2989d.jpg','all-products','selected-products'],
+      38:['forMen','men-watches','name',33,44,'/w-6.674fd88d.jpg','all-products','selected-products'],
+
+      38:['forWomen','women-clothes','name',33,44,'/c-1.792191ae.jpg','all-products'],
+      39:['forWomen','women-clothes','name',33,44,'/c-2.bdcfd1f8.jpg','all-products'],
+      40:['forWomen','women-clothes','name',33,44,'/c-3.1e0d6a4f.jpg','all-products'],
+      41:['forWomen','women-clothes','name',33,44,'/c-4.a512c2cd.jpg','all-products'],
+      42:['forWomen','women-clothes','name',33,44,'/c-5.c1ae39eb.jpg','all-products','selected-products'],
+      43:['forWomen','women-clothes','name',33,44,'/c-6.7218b803.jpg','all-products','selected-products'],
+      44:['forWomen','women-clothes','name',33,44,'/c-7.77c508c5.jpg','all-products','selected-products'],
+      45:['forWomen','women-clothes','name',33,44,'/c-8.bb81a16b.jpg','all-products','selected-products'],
+
+      46:['forWomen','women-glasses','name',33,44,'/g-1.ff7da5bd.jpg','all-products'],
+      47:['forWomen','women-glasses','name',33,44,'/g-2.92c43071.jpg','all-products'],
+      48:['forWomen','women-glasses','name',33,44,'/g-3.7632b512.jpg','all-products','selected-products'],
+      49:['forWomen','women-glasses','name',33,44,'/g-4.e779441a.jpg','all-products','selected-products'],
+      50:['forWomen','women-glasses','name',33,44,'/g-5.6ae21043.jpg','all-products'],
+
+      51:['forWomen','women-jewelry','name',33,44,'/j-1.759f1def.jpg','all-products'],
+      52:['forWomen','women-jewelry','name',33,44,'/j-2.356c4b49.jpg','all-products'],
+      53:['forWomen','women-jewelry','name',33,44,'/j-3.1645f598.jpg','all-products','selected-products'],
+      54:['forWomen','women-jewelry','name',33,44,'/j-4.a4b05ef6.jpg','all-products','selected-products'],
+      55:['forWomen','women-jewelry','name',33,44,'/j-5.a1a12eb1.jpg','all-products','selected-products'],
+
+      56:['forWomen','women-shoes','name',33,44,'/boots-1.7ed7e4d4.jpg','all-products'],
+      57:['forWomen','women-shoes','name',33,44,'/heels-1.04d558ea.webp','all-products'],
+      58:['forWomen','women-shoes','name',33,44,'/heels-3.dffa90fb.jpeg','all-products'],
+      59:['forWomen','women-shoes','name',33,44,'/nike-1.b17e7723.jpeg','all-products'],
+      60:['forWomen','women-shoes','name',33,44,'/nike-1.09061d67.webp','all-products'],
+      61:['forWomen','women-shoes','name',33,44,'/nike-2.b4a97f95.jpeg','all-products'],
+      62:['forWomen','women-shoes','name',33,44,'/puma-1.de7524e5.jpeg','all-products'],
+      63:['forWomen','women-shoes','name',33,44,'/puma-2.cf5c154a.jpg','all-products','selected-products'],
+      64:['forWomen','women-shoes','name',33,44,'/puma-3.5400338e.jpeg','all-products','selected-products'],
+      65:['forWomen','women-shoes','name',33,44,'/tenis-1.8f03e2d3.jpeg','all-products','selected-products'],
+      66:['forWomen','women-shoes','name',33,44,'/tenis-2.c02a518d.jpeg','all-products','selected-products'],
+      67:['forWomen','women-shoes','name',33,44,'/vans-1.b7de24e2.jpg','all-products','selected-products'],
+      68:['forWomen','women-shoes','name',33,44,'/vans-2.3a16e341.webp','all-products'],
+
+      69:['forWomen','women-watches','name',33,44,'/w-1.505a5bb4.jpg','all-products'],
+      70:['forWomen','women-watches','name',33,44,'/w-2.7d3dccbf.jpg','all-products'],
+      71:['forWomen','women-watches','name',33,44,'/w-3.7549dea7.jpg','all-products','selected-products'],
+      72:['forWomen','women-watches','name',33,44,'/w-4.2ec485c1.jpg','all-products','selected-products'],
+      73:['forWomen','women-watches','name',33,44,'/w-5.0b7963fd.jpg','all-products','selected-products'],
+
+
+
+
+
+
+
+  
+
+
 
 }
 
